1e94c3bee95634ee365cf3ead81d376eb3a34ca2
muzaddidah <127376280+muzaddidah@users.noreply.github.com>
2023-06-20 12:25:24 +0700

    Add files via upload

---
+++ layouts/dashboard.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>TUGAS 2 | Website Lanjutan</title>
+	<link rel="shortcut icon" href="<?php echo AST; ?>/img/star.ico" type="image/x-icon">
+	<link rel="stylesheet" href="<?php echo AST; ?>/css/style.css">
+</head>
+
+<body>
+
+	<aside>
+
+		<header>
+			<img src="<?php echo AST; ?>/img/apel.PNG" class="brand">
+			<div class="user">Caya</div>
+		</header>
+
+		<nav>
+			<ul>
+				<li>
+					<a href="<?php echo URL; ?>/dashboard">
+						<img class="icon" src="<?php echo AST; ?>/img/home.png" alt=""> Home
+					</a>
+				</li>
+				<li>
+					<a href="<?php echo URL; ?>/user">
+						<img class="icon" src="<?php echo AST; ?>/img/user.png" alt=""> User
+					</a>
+				</li>
+				<li>
+					<a href="<?php echo URL; ?>/golongan">
+						<img class="icon" src="<?php echo AST; ?>/img/gol.png" alt=""> Golongan
+					</a>
+				</li>
+				<li>
+					<a href="<?php echo URL; ?>/pelanggan">
+						<img class="icon" src="<?php echo AST; ?>/img/MENU.png" alt=""> Pelanggan
+					</a>
+				</li>
+				<li>
+				<a href="<?php echo URL; ?>/index">
+					<img id="logout" class="icon" src="<?php echo AST; ?>/img/login.png" alt=""> LogOut
+				</a>
+				</li>
+			</ul>
+		</nav>
+
+	</aside>
+
+	<main>
+		<article>
+			<?php require_once ROOT . "app/views/" . $view . ".php"; ?>
+		</article>
+
+		<footer>
+			Copyright &copy; 2023. Designed by Cahaya Muzaddidah
+		</footer>
+	</main>
+
+</body>
+
+</html>
